a50b5a69444df82099f88cf331e730eed98e6da7
herold1989 <jens_herold@gmx.net>
2022-12-04 14:44:13 +0100

    add update profil page

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,43 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+                src="https://media-exp1.licdn.com/dms/image/C4D03AQGQxEHObPdKPw/profile-displayphoto-shrink_800_800/0/1521378950303?e=2147483647&v=beta&t=xWKvfpVbtvHdA6OOKLRJN4Mj40TDBE8ns5Hw7oPGoL0"
+                alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsPPInput"
+            />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Safak" name="name" />
+          <label>Email</label>
+          <input type="email" placeholder="safak@gmail.com" name="email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
