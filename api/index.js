ec6841523578a40d58e360134e198e57f0306fdf
herold1989 <jens_herold@gmx.net>
2023-07-12 19:49:05 +0200

    add password encryption

--- api/index.js
+++ api/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json())
+
+
 mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("Connected to MongoDB"))
 .catch(err=>console.log(err));
 
-app.use("/jens", (req, res) =>{
-    console.log("Hey this is Jens")
-})
+app.use("/api/auth", authRoute)
 
 app.listen("8080", ()=>{
     console.log("Backend is running.");
