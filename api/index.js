c728f74424f16ac6453051781fd664553086b3b7
herold1989 <jens_herold@gmx.net>
2023-07-12 17:26:19 +0200

    connect to localhost

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use("/jens", (req, res) =>{
+    console.log("Hey this is Jens")
+})
+
 app.listen("8080", ()=>{
     console.log("Backend is running.");
 })
