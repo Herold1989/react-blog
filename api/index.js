7f127c1219b47d74878bd34b69d577208b2956fd
herold1989 <jens_herold@gmx.net>
2023-07-16 22:15:22 +0200

    fixed update routine

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
-const userRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 
 dotenv.config();
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch(err=>console.log(err));
 
 app.use("/api/auth", authRoute)
-app.use("/api/users",userRoute)
+app.use("/api/users", userRoute)
 
 
 app.listen("8080", ()=>{
