3997470f6c7b8b2b301e2a338b21cba558bfa972
herold1989 <jens_herold@gmx.net>
2023-07-17 20:11:46 +0200

    start linking front- and backend

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,24 +1,28 @@
 import "./post.css"
+import { Link } from "react-router-dom"
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-        <img className="postImg"         
-        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
+      {post.photo && (
+        <img className="postImg"
+          src={post.photo}
+          alt=""
         />
-    <div className="postInfo">
-        <div className="postCats">
-            <span className="postCat">Music</span>
-            <span className="postCat">Life</span>
+      )}
+      <div className="postInfo">
+        <div className="postCats">{
+          post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">Lorem ipsum dolor sit amet.</span>
-        <hr/>
-        <span className="postDate">
-            1 hour ago
-        </span>
-    </div>
-    <p className="postDesc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, optio distinctio deleniti quaerat id velit, autem hic architecto rem sit saepe laborum ipsam eius explicabo nesciunt aperiam natus ipsum a! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, optio distinctio deleniti quaerat id velit, autem hic architecto rem sit saepe laborum ipsam eius explicabo nesciunt aperiam natus ipsum a.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, optio distinctio deleniti quaerat id velit, autem hic architecto rem sit saepe laborum ipsam eius explicabo nesciunt aperiam natus ipsum a. </p>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+      </div>
+      <p className="postDesc">{post.description}</p>
     </div>
   )
 }
