904646ddb50fb1f408b5fc1a0ac4a4c478215017
herold1989 <jens_herold@gmx.net>
2023-07-17 16:17:01 +0200

    get post entries from MongoDB

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -38,17 +38,32 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE POST
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.username === req.body.username) {
+        try {
+          await post.deleteOne();
+          res.status(200).json("Post has been deleted...");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(401).json("You can delete only your post!");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 //GET POST
 router.get("/:id", async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
 });
-
 
 module.exports = router;
