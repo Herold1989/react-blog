dfcfa42670e1ea195e43f0e0094445e6f8ad833e
herold1989 <jens_herold@gmx.net>
2023-07-17 20:36:25 +0200

    connected blog posts with database

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,32 +1,42 @@
-import "./singlepost.css"
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./singlepost.css";
 
 export default function SinglePost() {
+  const location = useLocation()
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/"+path)
+      setPost(res.data)
+    };
+    getPost();
+  },[path])
+
   return (
     <div className="singlePost">
         <div className="singlePostWrapper">
+          {post.photo &&(
         <img
-          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src={post.photo}
           alt=""
-          className="singlePostImg"/>   
-          <h1 className="singlePostTitle">Lorem ipsum dolor sit amet.
+          className="singlePostImg"/>
+          )}
+          <h1 className="singlePostTitle">
+            {post.title}
           <div className="singlePostEdit">
             <i className ="singlePostIcon far fa-edit"></i>
-            <i class="singlePostIcon far fa-trash-alt"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
           </div>
           </h1> 
           <div className="singlePostInfo">
-            <span className = "singlePostAuthor">Author: <b>Jens</b></span>
-            <span className = "singlePostDate"> 1 hour ago</span>
+            <span className = "singlePostAuthor">Author: <b>{post.username}</b></span>
+            <span className = "singlePostDate">{new Date(post.createdAt).toDateString()}</span>
           </div>
-          <p className="singlePostDesc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet facere voluptates, tempore libero eaque, deleniti reprehenderit at nobis iste omnis quibusdam, provident corrupti. Asperiores nisi reprehenderit, totam officiis voluptatibus facere.
+          <p className="singlePostDesc">{post.description}
           </p>
         </div>
     </div>
