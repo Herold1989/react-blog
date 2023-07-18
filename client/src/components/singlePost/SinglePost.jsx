16ef0c3a801ba70eae37480bbbd656f2290931bf
herold1989 <jens_herold@gmx.net>
2023-07-18 09:58:27 +0200

    linked categories to MongoDB

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./singlepost.css";
@@ -33,7 +33,9 @@ export default function SinglePost() {
           </div>
           </h1> 
           <div className="singlePostInfo">
-            <span className = "singlePostAuthor">Author: <b>{post.username}</b></span>
+            <span className = "singlePostAuthor">Author:  
+            <Link to ={`/?user=${post.username}`} className="link"><b>{post.username}</b></Link>
+            </span>
             <span className = "singlePostDate">{new Date(post.createdAt).toDateString()}</span>
           </div>
           <p className="singlePostDesc">{post.description}
