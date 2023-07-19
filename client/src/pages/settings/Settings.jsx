1314bcc3c4b15ef9be5cf7c0846a6256d534d0c8
herold1989 <jens_herold@gmx.net>
2023-07-19 14:13:25 +0200

    went back to older versions

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,95 +1,46 @@
 import "./settings.css";
 import Sidebar from "../../components/sidebar/Sidebar";
-import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
-import axios from "axios";
+import { useContext } from "react";
+
 
 export default function Settings() {
-  const [file, setFile] = useState(null);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [success, setSuccess] = useState(false);
-
-  const { user, dispatch } = useContext(Context);
+  const { user } = useContext(Context)
   const PF = "http://localhost:8080/images/"
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    dispatch({ type: "UPDATE_START" });
-    const updatedUser = {
-      userId: user._id,
-      username,
-      email,
-      password,
-    };
-    if (file) {
-      const data = new FormData();
-      const filename = Date.now() + file.name;
-      data.append("name", filename);
-      data.append("file", file);
-      updatedUser.profilePicture = filename;
-      try {
-        await axios.post("/upload", data);
-      } catch (err) {}
-    }
-    try {
-      const res = await axios.put("/users/" + user._id, updatedUser);
-      setSuccess(true);
-      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
-    } catch (err) {
-      dispatch({ type: "UPDATE_FAILURE" });
-    }
-  };
   return (
     <div className="settings">
       <div className="settingsWrapper">
         <div className="settingsTitle">
-          <span className="settingsUpdateTitle">Update Your Account</span>
-          <span className="settingsDeleteTitle">Delete Account</span>
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
         </div>
-        <form className="settingsForm" onSubmit={handleSubmit}>
+        <form className="settingsForm">
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img
-              src={file ? URL.createObjectURL(file) : PF+user.profilePicture}
-              alt=""
+                src={PF + user.profilePicture}
+                alt=""
             />
             <label htmlFor="fileInput">
-              <i className="settingsPPIcon far fa-user-circle"></i>
+              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
             </label>
             <input
-              type="file"
               id="fileInput"
+              type="file"
               style={{ display: "none" }}
-              onChange={(e) => setFile(e.target.files[0])}
+              className="settingsPPInput"
             />
           </div>
           <label>Username</label>
-          <input
-            type="text"
-            placeholder={user.username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <input type="text" placeholder="Jens" name="name" />
           <label>Email</label>
-          <input
-            type="email"
-            placeholder={user.email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <input type="email" placeholder="jens.herold1989@gmail.com" name="email" />
           <label>Password</label>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className="settingsSubmit" type="submit">
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
             Update
           </button>
-          {success && (
-            <span style={{ color: "green", textAlign: "center", marginTop: "20px" }}>
-              Profile has been updated...
-            </span>
-          )}
         </form>
       </div>
       <Sidebar />
